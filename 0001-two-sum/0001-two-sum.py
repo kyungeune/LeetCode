b195b943e67462a6d4872be80f4af56a45605072
kyungeune <132311633+kyungeune@users.noreply.github.com>
2025-05-17 22:45:31 +0900

    Time: 4207 ms (5%), Space: 18.5 MB (79.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,5 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # answer = [0]*2
-        # visited=[0]*len(max(nums))
-        # nums = sorted(nums)
-        # print(nums)
-        # 논리 : sort한 후, visited는 방문하지 않고, 정답 나오는 순간 break
         idx = []
         temp=0
         for i in range(len(nums)):
