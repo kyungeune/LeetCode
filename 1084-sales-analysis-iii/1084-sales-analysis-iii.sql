5d0dd910651221ca41f484f569a0c8c9bb659ef8
kyungeune <132311633+kyungeune@users.noreply.github.com>
2026-01-07 17:17:57 +0900

    Time: 1240 ms (20.8%), Space: 0B (100%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT S.product_id, P.product_name
+FROM Product P, Sales S
+WHERE S.sale_date BETWEEN '2019-01-01' AND '2019-03-31' AND P.product_id = S.product_id AND P.product_id NOT IN (SELECT product_id FROM Sales WHERE sale_date<'2019-01-01' OR sale_date>'2019-03-31')
+GROUP BY S.product_id
+
+
+-- SELECT P.product_id, P.product_name
+-- FROM Product P, Sales S
+-- WHERE S.sale_date BETWEEN '2019-01-01' AND '2019-03-31' AND P.product_id NOT IN 
+-- GROUP BY P.product_id
+-- HAVING COUNT(P.product_id) > 1;
