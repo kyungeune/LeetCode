d46242a75c157fb4ff6fae2e2019b317c1b8b8b5
kyungeune <132311633+kyungeune@users.noreply.github.com>
2026-01-09 08:27:35 +0900

    Time: 558 ms (7.23%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,7 +1,9 @@
 SELECT DISTINCT author_id AS id
 FROM Views
 WHERE author_id = viewer_id
+
 # 런타임 400 ms
--- ORDER BY id ASC;
-# 런타임 
-ORDER BY author_id ASC;
+ORDER BY id ASC;
+
+# 런타임 413 ms
+-- ORDER BY author_id ASC;
