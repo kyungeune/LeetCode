27a1408ebd93af2cf99106e6773aadd26029339b
kyungeune <132311633+kyungeune@users.noreply.github.com>
2026-01-08 19:14:03 +0900

    Time: 523 ms (36.47%), Space: 19.4 MB (16.12%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -6,18 +6,23 @@ class Solution:
         
         res = ""
         
-        # 홀수 길이 ex) a aba
+        # 홀수 길이씩 순회합니다. ex) a aba
         for i in range(len(s)):
+            # l : left, r : right
             l, r = i, i
-            while l>=0 and r<len(s) and s[l] == s[r]:
+            
+            # l은 계속 작아지고, r은 계속 커지며 양 옆이 같은 경우만 palindrome로 판별
+            while l >= 0 and r < len(s) and s[l] == s[r]:
                 word = s[l:r+1]
+                
+                # 만약 단어가 기존 res보다 길다면
                 if len(word) > len(res):
                     res = word
                 l-=1
                 r+=1
 
 
-        # 짝수 길이 ex) abba
+        # 짝수 길이로 순회합니다. ex) abba
         for i in range(len(s)):
             l, r = i, i+1
             while l>=0 and r<len(s) and s[l] == s[r]:
