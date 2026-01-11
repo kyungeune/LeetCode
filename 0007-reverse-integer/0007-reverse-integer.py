15deb99b92eb2fbc670bf24f8c2fcd300c6d2328
kyungeune <132311633+kyungeune@users.noreply.github.com>
2026-01-12 08:11:24 +0900

    Time: 50 ms (6.47%), Space: 19.3 MB (6.62%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,29 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        # 음수인지 확인
+        minus = 0
+        # 반환할 값
+        rslt = 0
+        
+        # 음수이면, 맨 앞 -를 제외해주기
+        if x < 0:
+            minus = 1
+        
+        # x를 양수로 변환
+        x = abs(x)
+
+        # 순서를 반대로 넣기
+        while x != 0:
+            rslt *= 10
+            rslt += x % 10
+            x //= 10
+
+        # 원래 음수였던 값은 양수로 변환
+        if minus == 1:
+            rslt = -rslt
+
+        # 제출 가능 범위 필터링
+        if rslt < -2**31 or rslt > 2**31 - 1:
+            return 0
+        
+        return rslt
