89c22a34cf76bebedde23f4fcb3f5349d912d67e
kyungeune <132311633+kyungeune@users.noreply.github.com>
2026-01-19 08:54:02 +0900

    Time: 479 ms (81.78%), Space: 22.2 MB (23.34%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -2,44 +2,39 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         rslt = []
         nums.sort()
+        n = len(nums)
 
-
-        # 함수 정의 for 효율성
-        def skip_forward(m: int, r: int) -> int:
-            while m < r and nums[m] == nums[m - 1]:  # 중복 제거
-                m += 1
-            return m
-        
-        def skip_backward(m: int, r: int) -> int:
-            while m < r and nums[r] == nums[r + 1]:  # 중복 제거
-                r -= 1
-            return r
-
-
-        for i in range(len(nums) - 1):
-            # Two Pointer 활용
-            l = i  # 고정
-            m = i + 1
-            r = len(nums) - 1
-            
+        for i in range(n - 2):
             # 중복 제거 / 맨 처음은 예외 (ex. input : [0,0,0])
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
+            # 시간 줄이기 1 : 가지치기
+            if nums[i] > 0:
+                break
 
-            while i < m and m < r:
+            # Two Pointer 활용
+            l = i  # 고정
+            m = i + 1
+            r = n - 1
+
+            # 시간 줄이기 2 : 조건 바꾸기
+            while m < r: 
                 total = nums[l] + nums[m] + nums[r]
 
                 if total == 0:
                     rslt.append([nums[l], nums[m], nums[r]])
                     m += 1  # 무한 roop 방지
                     r -= 1
-                    m = skip_forward(m, r)
-                    r = skip_backward(m, r)
+
+                    # ✅ m, r 중복 스킵 (정답 찾았을 때만)
+                    while m < r and nums[m] == nums[m - 1]:
+                        m += 1
+                    while m < r and nums[r] == nums[r + 1]:
+                        r -= 1
+                
                 elif total < 0:
                     m += 1
-                    m = skip_forward(m, r)
                 else:
                     r -= 1
-                    r = skip_backward(m, r)
-        
+                    
         return rslt
