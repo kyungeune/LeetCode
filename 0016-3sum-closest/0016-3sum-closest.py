65c3cbdc635ea4ee85192229f7a207e5925136ad
kyungeune <132311633+kyungeune@users.noreply.github.com>
2026-01-20 08:43:13 +0900

    Time: 375 ms (36.52%), Space: 19.5 MB (12.45%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -4,10 +4,12 @@ class Solution:
         nums.sort()
         rslt = 0
         
-
         for i in range(len(nums) - 2):
+            # two pointer
             l = i + 1
             r = len(nums) - 1
+
+            # 중복 제거
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
             
@@ -15,12 +17,13 @@ class Solution:
                 total = nums[i] + nums[l] + nums[r]
                 diff = abs(total - target)
                 
+                # 최소 차이를 도달한 경우
                 if min_diff > diff:
                     min_diff = diff
-                    print("min_diff:",min_diff, " i,l,r",i,l,r,"\n")
+                    # print("min_diff:",min_diff, " i,l,r",i,l,r,"\n")
                     rslt = total
                 
-
+                # pointer 이동
                 if total < target:
                     l += 1
                 elif total > target:
