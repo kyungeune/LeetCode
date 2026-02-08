807d5aeb2ab7b8e4ff4f269c8a35b49b9465a33f
kyungeune <132311633+kyungeune@users.noreply.github.com>
2026-02-09 08:30:30 +0900

    Time: 301 ms (17.96%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,5 +1,5 @@
 SELECT (  # 내부에서 하나도 반환되지 않으면 NULL로 표시
-    SELECT DISTINCT(SALARY)
+    SELECT DISTINCT(SALARY)  # 같은 값 두개 이상인 경우 방지
     FROM EMPLOYEE
     ORDER BY SALARY DESC
     LIMIT 1 OFFSET 1  # 1개, 앞에서 하나 건너뛴
